11c6b2263bcc1a049a3e5126a3e065b9ba05d661
Riviere Théotime <theotimeriviere@gmail.com>
2015-08-08 14:52:06 +0200

    Add header

--- Tools/setup.sh
+++ Tools/setup.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+###############################################################
+#     ####         #                               #          # Jobuzzle - Copyright All rights reserved
+#     ####         #                               #          #
+#       ##  #####  ######  #     # ####### ####### #  #####   # @Author: revers
+#       ## #     # #     # #     #      #       #  # #     #  #
+#       ## #     # #     # #     #     #       #   # #     #  # @Date:   2015-05-22 17:00:05
+#       ## #     # #     # #     #    #       #    # #     #  #
+#       ## #     # #     # #     #   #       #     # ######   # @Last Modified by:   revers
+#  ####### #     # #     # #     #  #       #      # #        #
+#  ######   #####   ######  #####  ####### ####### # #######  # @Last Modified time: 2015-08-08 14:36:44
+###############################################################
+
+
 git submodule init
 git submodule update
 git submodule foreach --recursive git checkout master
