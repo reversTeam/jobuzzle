e8268f157aa0f6b77db1710039b1e4d657fa10c2
Riviere Théotime <theotimeriviere@gmail.com>
2015-09-13 13:57:18 +0200

    Remove watcher and manage by puzzle

--- Tools/staging/setup-front.sh
+++ Tools/staging/setup-front.sh
@@ -8,10 +8,10 @@
 #       ## #     # #     # #     #    #       #    # #     #  #
 #       ## #     # #     # #     #   #       #     # ######   # @Last Modified by:   revers
 #  ####### #     # #     # #     #  #       #      # #        #
-#  ######   #####   ######  #####  ####### ####### # #######  # @Last Modified time: 2015-08-08 14:36:39
+#  ######   #####   ######  #####  ####### ####### # #######  # @Last Modified time: 2015-09-13 13:56:40
 ###############################################################
 
 
 puzzle git current --recursive --skip-base branch move staging
-(cd Watcher && node main 30)
+puzzle watcher --oneshot
 cp Tools/staging/.htaccess Build/client/
