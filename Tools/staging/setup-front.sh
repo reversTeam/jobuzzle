bec3a93e296063f3b4495ecf80423fa9a2021c72
Riviere Théotime <theotimeriviere@gmail.com>
2015-10-02 20:30:46 +0200

    Fix setup

--- Tools/staging/setup-front.sh
+++ Tools/staging/setup-front.sh
@@ -8,11 +8,11 @@
 #       ## #     # #     # #     #    #       #    # #     #  #
 #       ## #     # #     # #     #   #       #     # ######   # @Last Modified by:   revers
 #  ####### #     # #     # #     #  #       #      # #        #
-#  ######   #####   ######  #####  ####### ####### # #######  # @Last Modified time: 2015-10-02 20:28:15
+#  ######   #####   ######  #####  ####### ####### # #######  # @Last Modified time: 2015-10-02 20:30:41
 ###############################################################
 
 
 puzzle git current --recursive --skip-base branch move staging
-git submodules foreach git pull origin staging
+git submodule foreach git pull origin staging
 puzzle watcher --oneshot
 cp Tools/staging/.htaccess Build/client/
